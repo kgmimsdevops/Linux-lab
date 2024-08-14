c4075c6b705788475855c2eba2f64903b7a60af3
bharathi-13 <sathyabharathi00131@gmail.com>
2024-08-14 16:29:04 +0530

    Playbook was not found and where is ansible.cfg

--- lab.py
+++ lab.py
@@ -1,3 +1,6 @@
 import ansible_runner
 
-print(dir(ansible_runner))
+r = ansible_runner.run(playbook='main.yaml', private_data_dir='./Ansible-Runner/')
+# successful: 0
+print("Final status:")
+print(r.stats)
